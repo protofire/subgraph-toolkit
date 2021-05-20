cd2e88ae8c74bc9146bf640aa7c1242ad2758040
Sebastián Galiano <sgaliano@gmail.com>
2021-05-20 09:57:34 -0300

    Add ETH token address

--- lib/constants.ts
+++ lib/constants.ts
@@ -9,6 +9,8 @@ export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 export const GENESIS_ADDRESS = ADDRESS_ZERO
 export const ZERO_ADDRESS = ADDRESS_ZERO
 
+export const ETH_TOKEN_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+
 // Decimals
 export const DEFAULT_DECIMALS = 18
 
